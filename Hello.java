275414124619fc4ed1fb0df312acc3b04068c432
Yash <yashtyagi395@gmail.com>
2025-05-11 17:10:50 +0530

    This is the First Commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java This is the First Class of Java");
+        System.out.print("Yash ");
+        System.out.print("Tyagi");
+        System.out.println("sjd");
+        System.out.printf("%d", 10);
+    }
+}
